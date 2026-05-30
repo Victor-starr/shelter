066a720517134ba7a16b07f665e076066550f721
Victor <v94625230@gmail.com>
2026-05-30 16:45:12 +0300

    Update site metadata, add Supabase dependencies, and remove unused SVG files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "thumbs.dreamstime.com",
+      "live.staticflickr.com",
+      "www.aspca.org",
+    ],
+  },
 };
 
 export default nextConfig;
