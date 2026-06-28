cf4b53b71bb84e8f0bc72d7526188db5e2a00abf
Victor <v94625230@gmail.com>
2026-06-28 20:48:17 +0300

    Update next.config.ts; restrict image domains to Supabase only

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: [
-      "thumbs.dreamstime.com",
-      "live.staticflickr.com",
-      "www.aspca.org",
-    ],
+    domains: ["ftbuknckuveqtlrsntmi.supabase.co"],
   },
 };
 
